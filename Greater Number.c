7c63a5088d77c7fb4ae1bb10769cf4b59fcb6b1a
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2022-11-22 00:30:09 -0800

    Find a greater number.

---
+++ Greater Number.c
@@ -0,0 +1,17 @@
+// A program for simple if else statement.
+
+#include <stdio.h>
+
+int main()
+{
+    int a=15;
+    if(a>20)
+    {
+        printf("A is greater than 20");
+    }
+    {
+    else
+        printf("20 is greater than A");
+    }
+    return 0;
+}
