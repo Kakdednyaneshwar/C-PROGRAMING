8e8000f17ea17979abb81158385d9fab31ebd5ca
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2022-11-22 00:10:08 -0800

    Add files via upload

---
+++ One array Element.c
@@ -0,0 +1,17 @@
+//ONE DIMENSIONAL ARRAY ELEMENT.
+#include <stdio.h>
+
+int main()
+{
+    int x[5]={20,40,60,80,100};
+    int i;
+    printf("x element\n");
+    for(i=0;i<5;i++)
+    {
+        printf("%d\t",x[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
