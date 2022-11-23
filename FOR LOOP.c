e9190e82fd2ca192be7c98b18d1e7028c9d881e4
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2022-11-23 00:59:06 -0800

    Star print using for loop..

---
+++ FOR LOOP.c
@@ -0,0 +1,18 @@
+//A program for print star using the for loop....
+#include <stdio.h>
+
+int main()
+{
+    int i,j;
+    for(i=30;i>=1;i--)
+    
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+   
+      return 0;
+}
