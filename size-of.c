205af83a23cd37ebdc6778ca74be95103592f1a6
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2022-11-23 22:42:00 -0800

    size of data types(int,char,float,double..

---
+++ size-of.c
@@ -0,0 +1,13 @@
+//A program for size of data types...
+#include <stdio.h>
+#include<limits.h>
+
+int main()
+{
+    printf("Size of int is=%ld\n",sizeof(int));
+    printf("Size of char is=%ld\n",sizeof(char));
+    printf("Size of float is=%ld\n",sizeof(float));
+    printf("Size of double is=%ld",sizeof(double));
+
+    return 0;
+}
