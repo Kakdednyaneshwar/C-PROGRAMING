e9ecd055f9e305d0e2a81093bc3fea065f0c16c0
Kakde Dnyaneshwar <118787516+Kakdednyaneshwar@users.noreply.github.com>
2022-11-23 22:39:25 -0800

    conditional-op-?,:/;.

---
+++ conditional-operator.c
@@ -0,0 +1,13 @@
+//A program for conditional operator(?,:,;)..
+#include <stdio.h>
+
+int main()
+{
+    int mark;
+    printf("Enter mark=");
+    scanf("%d",&mark);
+    mark>35? printf("Pass"):printf("Fail");
+    
+
+    return 0;
+}
